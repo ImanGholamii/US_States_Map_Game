d192a8969e8802d8ec69b87f5a75d054d6c908de
Iman <imangholamiimi@gmail.com>
2024-08-14 19:46:32 +0330

    :sparkles: | ADDED | make_question function & while loop to repeat question

--- main.py
+++ main.py
@@ -6,4 +6,15 @@ image = "blank_states_img.gif"
 screen.addshape(image)  # path of image
 turtle.shape(image)
 
+def make_question():
+    """Ask user state guess"""
+    user_input = screen.textinput(title="Guess states name", prompt="What's another state name?")
+    return user_input.title()
+
+
+game_is_on = True
+while game_is_on:
+    state_name = make_question()
+    pass
+
 screen.exitonclick()
