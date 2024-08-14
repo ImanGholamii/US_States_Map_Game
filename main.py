ddd85e3791f3031d896e2a1ed71de45ef9851875
Iman <imangholamiimi@gmail.com>
2024-08-14 20:59:28 +0330

    :sparkles: | ADDED | take_position function

--- main.py
+++ main.py
@@ -21,9 +21,19 @@ def check_answer(user_input):
     """check user input exist or not"""
     if data.state.str.contains(user_input).any():
         print(data[data.state == user_input])
+        return True
 
 
-game_is_on = True
+def take_position(user_input):
+    """Extract x, y coordinate from data file"""
+    filtered_data = data[data.state == user_input]
+    x_coord = int(filtered_data.x)
+    y_coord = int(filtered_data.y)
+    coordinates = (x_coord, y_coord)
+    return coordinates
+
+
+game_is_on = False
 while game_is_on:
     state_name = make_question()
     check_answer(user_input=state_name)
